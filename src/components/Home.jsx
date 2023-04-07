1abc45a32601bfe987df0ff9ad5eaf07ba43ddf0
andrianggoro21 <andri.anggoro21@gmail.com>
2023-04-07 22:06:21 +0700

    add fitur footer untuk homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Carousel from 'react-bootstrap/Carousel';
@@ -6,64 +7,66 @@ import Navbar from '../components/Navbar/Navbar'
 import imgCorousel1 from '../assets/images/img-carousel-new1.png'
 import imgCorousel2 from '../assets/images/img-carousel-new2.png'
 import imgCorousel3 from '../assets/images/img-carousel-new3.png'
+import Footer from '../components/Footer/Footer'
 
 
 function Home() {
   return (
     <div className='home-page'>
-      <Navbar />
-      <div class="body-content-carousel">
-      <div class="container-fluid">
-      <Carousel>
-      <Carousel.Item>
-        <img className="d-block w-100" src={imgCorousel1} alt="First slide" />
-        <div class="carousel-btn d-flex align-items-center gap-3">
-          <a class="watch-now" href="./streaming.html">
-            <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
-              <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
-            </button>
-          </a>
-          <div class="trailer">
-            <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Thewitcher">
-                <span>Watch Trailer</span>
-            </button>
-          </div>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={imgCorousel2} alt="Second slide" />
-        <div class="carousel-btn d-flex align-items-center gap-3">
-          <a class="watch-now" href="./streaming.html">
-            <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
-              <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
-            </button>
-          </a>
-          <div class="trailer">
-            <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Vikingsvalhalla">
+    <Navbar />
+    <div class="body-content-carousel">
+    <div class="container-fluid">
+    <Carousel>
+    <Carousel.Item>
+      <img className="d-block w-100" src={imgCorousel1} alt="First slide" />
+      <div class="carousel-btn d-flex align-items-center gap-3">
+        <a class="watch-now" href="./streaming.html">
+          <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
+            <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
+          </button>
+        </a>
+        <div class="trailer">
+          <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Thewitcher">
               <span>Watch Trailer</span>
-            </button>
-          </div>
+          </button>
         </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={imgCorousel3} alt="Third slide" />
-        <div class="carousel-btn d-flex align-items-center gap-3">
-          <a class="watch-now" href="./streaming.html">
-            <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
-              <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
-            </button>
-          </a>
-          <div class="trailer">
-            <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Gameofthrones">
-              <span>Watch Trailer</span>
-            </button>
-          </div>
-        </div>
-      </Carousel.Item>
-    </Carousel>
       </div>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src={imgCorousel2} alt="Second slide" />
+      <div class="carousel-btn d-flex align-items-center gap-3">
+        <a class="watch-now" href="./streaming.html">
+          <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
+            <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
+          </button>
+        </a>
+        <div class="trailer">
+          <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Vikingsvalhalla">
+            <span>Watch Trailer</span>
+          </button>
+        </div>
+      </div>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src={imgCorousel3} alt="Third slide" />
+      <div class="carousel-btn d-flex align-items-center gap-3">
+        <a class="watch-now" href="./streaming.html">
+          <button type="button" class="btn btn-primary carousel-btn-play d-flex justify-content-center align-items-center gap-2">
+            <img class="w-auto" src="./assets/images/btn-watch.png" alt=""/><span>Watch Now </span>
+          </button>
+        </a>
+        <div class="trailer">
+          <button type="button" class="btn btn-outline-light btn-trailer d-flex justify-content-center align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modal-Gameofthrones">
+            <span>Watch Trailer</span>
+          </button>
+        </div>
+      </div>
+    </Carousel.Item>
+    </Carousel>
     </div>
-    </div>
+  </div>
+  <Footer/>
+  </div> 
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../../assets/css/styles.css'
